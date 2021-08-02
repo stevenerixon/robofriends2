41fec3331abf31f1d19ac141f292820f02bca8be
Steven Erixon <steven.erixon@learnet.se>
2021-08-03 01:45:14 +0200

    first commit

---
+++ src/robot.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Steven Erixon",
+    username: "Steven",
+    email: "steven@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Malin andersson",
+    username: "malin",
+    email: "malin@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Spaghetti Fornandes",
+    username: "Spaghetti",
+    email: "spaghetti@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Tomato Berlino",
+    username: "Tomato",
+    email: "tomato@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Nike Shoes",
+    username: "MrNike",
+    email: "nike@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Adidas all",
+    username: "Adidas",
+    email: "adidas@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Carl Hilton",
+    username: "Carl",
+    email: "carl@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Arigato Lmas",
+    username: "Arigato",
+    email: "arigato@gmail.com",
+  },
+  {
+    id: 9,
+    name: "MC Freestyler",
+    username: "MC",
+    email: "freestyler@gmail.com",
+  },
+  {
+    id: 10,
+    name: "Red Bandanda",
+    username: "red",
+    email: "red@gmail.com",
+  },
+];
